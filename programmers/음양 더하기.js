ba220f0c36cb34cd8524185bc1137b2f99c6b8f4
yeun38 <choh1997@naver.com>
2022-07-15 16:44:41 +0900

    음향 더하기_2

--- programmers/음양 더하기.js
+++ programmers/음양 더하기.js
@@ -1,13 +1,16 @@
 function solution(absolutes, signs) {
   var answer = 123456789;
   let sum = 0;
+  let new_array = [];
   for (let i = 0; i < absolutes.length; i++) {
     if (signs[i]) {
-      sum += absolutes[i];
+      new_array.push(absolutes[i] * 1);
     } else {
-      sum -= absolutes[i];
+      new_array.push(absolutes[i] * -1);
     }
   }
   answer = sum;
   return answer;
 }
+
+console.log(solution([1, 2, 3, 4], [true, false, false, true]));
