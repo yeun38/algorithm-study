b6f4f624b2c01abfaf2ed1ff0e448551dd9cd6ea
yeun38 <choh1997@naver.com>
2022-07-18 16:05:45 +0900

    완주하지 못한 선수

--- programmers/완주하지 못한 선수.js
+++ programmers/완주하지 못한 선수.js
@@ -1,15 +1,9 @@
 function solution(participant, completion) {
-  const map = new Map();
-
-  for (let i = 0; i < participant.length; i++) {
-    let a = participant[i],
-      b = completion[i];
-
-    map.set(a, (map.get(a) || 0) + 1);
-    map.set(b, (map.get(b) || 0) - 1);
-  }
-
-  for (let [k, v] of map) {
-    if (v > 0) return k;
+  participant.sort();
+  completion.sort();
+  for (var i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
+    }
   }
 }
