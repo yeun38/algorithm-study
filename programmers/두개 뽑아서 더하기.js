44f8dd56f1afb7ff92351d16563069e25220a619
yeun38 <choh1997@naver.com>
2022-07-20 14:49:25 +0900

    ✔ 두개 뽑아서 더하기

--- programmers/두개 뽑아서 더하기.js
+++ programmers/두개 뽑아서 더하기.js
@@ -2,11 +2,9 @@ function solution(numbers) {
   var answer = [];
   for (let i = 0; i < numbers.length - 1; i++) {
     for (let j = i + 1; j < numbers.length; j++) {
-      if (answer.includes(numbers[i] + numbers[j])) {
-        continue;
-      }
       answer.push(numbers[i] + numbers[j]);
     }
+    answer = [...new Set(answer)];
   }
   return answer.sort((a, b) => a - b);
 }
