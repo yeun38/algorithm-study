68417634d19ac9130feb678f89577b5e1b4eae88
yeun38 <choh1997@naver.com>
2022-07-20 13:38:56 +0900

    ✔ 제일 작은 수 제거하기

--- programmers/제일 작은 수 제거하기.js
+++ programmers/제일 작은 수 제거하기.js
@@ -1,15 +1,3 @@
-// function solution(arr) {
-//   var answer = [];
-//   if (arr.length == 1) {
-//     answer = [-1];
-//   } else {
-//     arr.splice(arr.indexOf(Math.min(arr)), 1);
-//     answer = arr;
-//   }
-//   return answer;
-// }
-
-console.log(solution([4, 5, 2, 1, 7]));
 function solution(arr) {
   var answer = [];
 
@@ -24,14 +12,3 @@ function solution(arr) {
 
   return answer;
 }
-
-// function solution(arr) {
-//   if (arr.length === 1) return [-1];
-
-//   let i = 0;
-//   for (let j = 0; j < arr.length; j += 1) {
-//     if (arr[i] > arr[j]) i = j;
-//   }
-//   arr.splice(i, 1);
-//   return arr;
-// }
