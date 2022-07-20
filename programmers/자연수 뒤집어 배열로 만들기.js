9f43235f530b2d450b7df470d47ca6214a6da12c
yeun38 <choh1997@naver.com>
2022-07-20 13:34:35 +0900

    ✔ 자연수 뒤집어 배열로 만들기

--- programmers/자연수 뒤집어 배열로 만들기.js
+++ programmers/자연수 뒤집어 배열로 만들기.js
@@ -1,36 +1,8 @@
-// function solution(arr) {
-//   var answer = [];
-//   let str = String(arr); // n = n + "" 스트링으로 변환
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     answer.push(Number(str[i]));
-//   }
-//   return answer;
-// }
-//return n.toString().split('').reverse().map(o => o = parseInt(o));
-// console.log(solution(12345));
-
-function solution(arr) {
-  var answer = [];
-  let str = String(arr);
-  for (let i = 0; i < str.length; i++) {
-    answer.push(Number(str[i]));
+function solution(n) {
+  let result = [];
+  while (1) {
+    result.push(n % 10);
+    n = Math.floor(n / 10);
+    if (n <= 0) return result;
   }
-  answer = answer.sort((a, b) => b - a);
-  console.log(typeof answer[1]);
-  return answer;
 }
-console.log(solution(12345));
-
-let result = [];
-while (1) {
-  result.push(n % 10);
-  n = Math.floor(n / 10);
-  if (n <= 0) return result;
-}
-
-// let sum = 0;
-// while(1) {
-// sum = (sum * 10) + n % 10
-// n = Math.floor(n / 10)
-// if(n <= 0) return sum
-// }
