74ab9254bd730bfb5d4219b1ba6e28aff8584a81
yeun38 <choh1997@naver.com>
2024-12-10 20:58:17 +0900

    241210

--- programmers/모의고사.js
+++ programmers/모의고사.js
@@ -1,25 +1,29 @@
 function solution(answers) {
-  var answer = [];
-  let arr1 = [1, 2, 3, 4, 5];
-  let arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  let arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const patterns = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
 
-  let count = [0, 0, 0];
+  const scores = [0, 0, 0];
 
-  for (i = 0; i < answer.length; i++) {
-    if (arr1[i % 5] == answer[i]) count[0]++;
-    if (arr2[i % 8] == answer[i]) count[1]++;
-    if (arr3[i % 10] == answer[i]) count[2]++;
-  }
-
-  let result = [];
-  const max = Math.max(...count);
-  let idx = 0;
-  for (let i = 0; i < count.length; i++) {
-    if (max == count[i]) {
-      result[idx] = i + 1;
-      idx++;
+  for (const [i, answer] of answers.entries()) {
+    for (const [j, pattern] of patterns.entries()) {
+      if (answer === pattern[i % pattern.length]) {
+        scores[j] += 1;
+      }
     }
   }
+
+  const max = Math.max(...scores);
+
+  const result = [];
+
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] === max) {
+      result.push(i + 1);
+    }
+  }
+
   return result;
 }
